b7204806dbfed428e9229879422de98f3fe40ab7
julius <julioglez.93@gmail.com>
2011-06-24 11:09:38 -0500

    added scripts

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,5 +2,5 @@
 #
 # $scrotwm: screenshot.sh,v 1.3 2009/09/13 22:28:53 marco Exp $
 
-scrot -cd5 '%d-%m-%Y_%H_%M_%S.png' -e 'mv $f ~/Images/myscreens'
+scrot -cd3 '%d-%m-%Y_%H_%M_%S.png' -e 'mv $f ~/Images/myscreens'
 
